7e670f51383d3c4b11067c2eaa0e28c5237dd61f
Seb Gertz <sebgertz@gmail.com>
2018-05-01 15:43:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // Write your code in this file!
-function scuberGreetingForFeet
+function scuberGreetingForFeet(feet)
+  if (feet > 2000) {
+    return "No can do."
+  } else if (feet >)
+
+    400) {
+    return "This one is on me!"
+  } else {
+    
+  }
+  
