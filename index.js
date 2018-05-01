ed20bf69d069eb78721aabd85d5732cb3a01a381
Seb Gertz <sebgertz@gmail.com>
2018-05-01 15:58:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet) {
+  if (feet > 2500) {
+    return "No can do."
+  } else if (feet > 2000) {
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return "This one is on me!"
+  }
+}
+
+function ternaryCheckCity(city){
+  let message;
+  city === "NYC" ? message = "Ok, sounds good." : message = "No go."
+  return message;
+}
+
+function switchOnCharmFromTip(tip) {
+  if (tip === "generous") {
+    return "Thank you so much."
+  } else if (tip === "not as generous") {
+    return "Thank you."
+  } else {
+    return "Bye."
+  }
+}
