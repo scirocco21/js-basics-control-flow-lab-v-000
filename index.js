f467b0369323a71f1ba3a1e36c66f73a175e3468
Seb Gertz <sebgertz@gmail.com>
2018-05-01 15:49:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ function scuberGreetingForFeet(feet) {
 }
 
 function ternaryCheckCity(city){
-  city === "NYC" ? "Ok, sounds good." : "No go."
+  city === "NYC" ? return "Ok, sounds good." : "No go."
 }
